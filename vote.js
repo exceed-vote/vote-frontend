41646c61756a4d791adcd88291ed90bfc4590ce7
Narongsak <narongsak.chobsri@gmail.com>
2017-07-21 11:08:24 +0700

    Add disable select and add function get option select

--- vote.js
+++ vote.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
   $.ajax({
-        url: 'http://localhost:8080/group/',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data)
-        }
-    })
-  console.log(Cookies.get("token"))
-  
+    url: 'http://localhost:8080/group',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+        var arr = $.map(data, function(el) { return el.name });
+        get_option(arr);
+    }
+  })
+
+  function get_option(data_name) {
+    $.each(data_name, function (i, name) {
+      $('#pop-selected').append($('<option>', { 
+          value: i,
+          text : name 
+        }));
+        console.log(name);
+    });
+  }
+
+  // console.log(Cookies.get("token"))
+    // console.log('2222');
 })
