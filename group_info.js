ce12933dd02665e9bc0b5a0137ace46fd83cf302
babestvl <babestvl@gmail.com>
2017-07-25 02:19:33 +0700

    Almost finish

--- group_info.js
+++ group_info.js
@@ -3,19 +3,40 @@ var appendData = function (data) {
     $(data).each(function (index, value) {
         if (count == 1) {
             $('#group-info').append('<div class="item active">' +  
-                '<img src="data:image/png;base64, ' + value.picture+ '" alt="group ' + count + '" class="img-responsive"/>' +
-                '<div class="carousel-caption">' +
-                '<h1>' + value.name + '</h1>' +
+                '<img src="/images/groupIMG/g' + count + '.jpg" alt="group ' + count + '" class="img-responsive group"/>' +
+                '<div class="center">' +
                 '<p>' + value.short_description + '</p>' +
                 '</div>' +
                 '</div>');
-                
         }
         else {
         $('#group-info').append('<div class="item">' +
-            '<img src="data:image/png;base64, ' + value.picture+ '" alt="group ' + count + '" class="img-responsive"/>' +
-            '<div class="carousel-caption">' +
-            '<h1>' + value.id + " " + value.name + '</h1>' +
+            '<img src="/images/groupIMG/g' + 1 + '.jpg" alt="group ' + count + '" class="img-responsive group"/>' +
+            '<div class="center">' +
+            '<p>' + value.short_description + '</p>' +
+            '</div>' +
+            '</div>');
+        }
+        count++;
+    });
+
+}
+
+var appendDataM = function (data) {
+    var count = 1;
+    $(data).each(function (index, value) {
+        if (count == 1) {
+            $('#group-info').append('<div class="item active">' +  
+                '<img src="/images/groupIMG/m' + count + '.jpg" alt="group ' + count + '" class="img-responsive group"/>' +
+                '<div class="center">' +
+                '<p>' + value.short_description + '</p>' +
+                '</div>' +
+                '</div>');
+        }
+        else {
+        $('#group-info').append('<div class="item">' +
+            '<img src="/images/groupIMG/m' + 1 + '.jpg" alt="group ' + count + '" class="img-responsive group"/>' +
+            '<div class="center">' +
             '<p>' + value.short_description + '</p>' +
             '</div>' +
             '</div>');
@@ -31,10 +52,18 @@ $(document).ready(function () {
         type: 'GET',
         url: 'http://localhost:8080/group',
     }).done(function (data) {
-        console.log(data);
-        appendData(data);
+        if ($(window).width() <= 992) {
+            appendDataM(data);
+        } else {
+            appendData(data);
+        }
     }).fail(function (data) {
         console.error(data)
         //show error
     })
+
+    $('.carousel').carousel({
+        interval: 3000
+    })    
+    
 })
